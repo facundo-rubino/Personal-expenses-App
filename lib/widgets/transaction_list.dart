fe90e260cebb69e1fcc157ba22ee1a279b37d2da
facundorubino21 <facundorubino21@gmail.com>
2023-01-26 13:01:06 -0300

    added scroll in list

--- lib/widgets/transaction_list.dart
+++ lib/widgets/transaction_list.dart
@@ -10,56 +10,61 @@ class TransactionList extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: transactions.map(((t) {
-        return Card(
-          child: Row(
-            children: <Widget>[
-              Container(
-                margin: EdgeInsets.symmetric(
-                  vertical: 10,
-                  horizontal: 15,
-                ),
-                decoration: BoxDecoration(
-                  border: Border.all(
-                    color: Colors.purple,
-                    width: 2,
-                  ),
-                ),
-                child: Text(
-                  '\$ ${t.amount}',
-                  style: TextStyle(
-                    fontWeight: FontWeight.bold,
-                    fontSize: 20,
-                    color: Colors.purple,
-                  ),
-                ),
-                padding: EdgeInsets.all(10),
-              ),
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
+    return Container(
+      height: 300,
+      child: SingleChildScrollView(
+        child: Column(
+          children: transactions.map(((t) {
+            return Card(
+              child: Row(
                 children: <Widget>[
-                  Text(
-                    t.title,
-                    style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                      fontSize: 17,
+                  Container(
+                    margin: EdgeInsets.symmetric(
+                      vertical: 10,
+                      horizontal: 15,
                     ),
-                  ),
-                  Text(
-                    DateFormat(
-                      'DD/mm/yyyy - HH:mm:ss',
-                    ).format(t.date),
-                    style: TextStyle(
-                      color: Colors.grey,
+                    decoration: BoxDecoration(
+                      border: Border.all(
+                        color: Colors.purple,
+                        width: 2,
+                      ),
                     ),
+                    child: Text(
+                      '\$ ${t.amount}',
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 20,
+                        color: Colors.purple,
+                      ),
+                    ),
+                    padding: EdgeInsets.all(10),
                   ),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      Text(
+                        t.title,
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 17,
+                        ),
+                      ),
+                      Text(
+                        DateFormat(
+                          'DD/mm/yyyy - HH:mm:ss',
+                        ).format(t.date),
+                        style: TextStyle(
+                          color: Colors.grey,
+                        ),
+                      ),
+                    ],
+                  )
                 ],
-              )
-            ],
-          ),
-        );
-      })).toList(),
+              ),
+            );
+          })).toList(),
+        ),
+      ),
     );
   }
 }
